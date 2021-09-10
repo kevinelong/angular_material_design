8341f17246d793c9cb91a42e79974ffe729b7256
kevinelong <kevinelong@gmail.com>
2021-09-10 15:26:42 -0700

    update play

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import {MatExpansionModule} from "@angular/material/expansion";
 import {MatBottomSheetModule} from "@angular/material/bottom-sheet";
 import {MatIconModule} from "@angular/material/icon";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatTableModule} from "@angular/material/table";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -22,7 +28,13 @@ import {MatToolbarModule} from "@angular/material/toolbar";
     MatExpansionModule,
     MatBottomSheetModule,
     MatIconModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatTabsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
